78372f2e631d9090067d9a21e322170c692598ce
Deniz Akar <denizakar2018@gmail.com>
2025-02-08 14:55:52 -0500

    First commit

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
 import Head from 'next/head'
 import Layout from '../components/layout';
-import ToDoList from '../components/todo-list';
+import PDFList from '../components/pdf-list';
+import styles from '../styles/layout.module.css'
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Full Stack Book To Do</title>
-        <meta name="description" content="Full Stack Book To Do" />
+        <title>Basic PDF CRUD App</title>
+        <meta name="description" content="Basic PDF CRUD App" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-        <ToDoList />
+        <PDFList />
       </Layout>
     </div>
   )
